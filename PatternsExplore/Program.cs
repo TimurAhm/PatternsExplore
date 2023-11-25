6406d5a6f0120d0bab6a6e340dba949f83c9aa8d
Timur <User@LAPTOP-Q3NBDU5U>
2023-11-25 16:43:35 +0400

    Add project files.

---
+++ PatternsExplore/Program.cs
@@ -0,0 +1,64 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Car car = new Car();
+        Horse horse = new Horse();
+        Bus bus = new Bus();
+
+
+        List<Vehicle> vehicleList = new List<Vehicle>();
+      //  vehicleList.Add(horse);
+        vehicleList.Add(car);
+        vehicleList.Add(bus);
+
+        for (int i = 0; i < vehicleList.Count; i++)
+        {
+            vehicleList[i].Move();
+        }
+
+    }
+}
+
+public interface IMovable
+{
+   void Move();
+}
+
+
+public abstract class Vehicle : IMovable
+{
+    public abstract void Move();
+}
+
+public class Car : Vehicle
+{
+    public override void Move()
+    {
+        Console.WriteLine("Car is moving");
+    }
+}
+
+public class Bus : Vehicle
+{
+    public override void Move()
+    {
+        Console.WriteLine("Bus is moving");
+    }
+}
+
+public class Horse : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Horse is moving");
+    }
+}
+
+public class Aiecraft : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("AirCraft is moving");
+    }
+}
